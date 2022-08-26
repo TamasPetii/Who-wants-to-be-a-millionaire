edb6370e23dab3fc0baaa4377358dab59008fe7d
Tamás Péter <60004480+TamasPetii@users.noreply.github.com>
2022-08-26 17:25:58 +0200

    Delete Visual Studio 2022 Version directory

--- Visual Studio 2022 Version/Time.cpp
+++
@@ -1,8 +0,0 @@
-#include "Time.h"
-
-std::string Time::To_String() {
-	int hour = secundum / 3600;
-	int min = secundum % 3600 / 60;
-	int sec = secundum % 3600 % 60;
-	return (hour < 10 ? "0" : "") + std::to_string(hour) + ":" + (min < 10 ? "0" : "") + std::to_string(min) + ":" + (sec < 10 ? "0" : "") + std::to_string(sec);
-}
